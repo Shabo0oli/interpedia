f6dea535b975bb4fa6a9629d2c1d9112e60cfe7f
shahabkarimi <shkarimi74@gmail.com>
2025-02-05 15:32:28 +0330

    fix config

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output: 'export', // Enable static export
+    basePath: '/interpedia', // Replace with your GitHub repo name
+    assetPrefix: '/interpedia/', 
+};
+
+module.exports = nextConfig
